ef1750f711650eb4895c75bb35c10d0a59d34920
Naren kumar Annadurai <149972628+klugzz@users.noreply.github.com>
2024-04-22 01:02:01 +0530

    Add files via upload

---
+++ Building simple Login and signup using py.py
@@ -0,0 +1,15 @@
+print('Create your account')
+
+username = input('Enter the Username: ')
+password = input('Enter the password: ')
+
+print('Your account was successfully created')
+print('Login to your account')
+
+username1 = input('Enter the username: ')
+password1 = input('Enter the password: ')
+
+if username == username1 and password == password1:
+    print('Login Successfully')
+else:
+    print('Invalid Credentials')
